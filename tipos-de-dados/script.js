34ba1f0c62e9d0ec79880df4647aa9cb3bda0edd
WanessaT <wanessatlsousa@gmail.com>
2024-07-07 16:28:31 -0300

    Aula sobre os tipos de dados e exercício usando strings

---
+++ tipos-de-dados/script.js
@@ -0,0 +1,17 @@
+var nome = 'Wanessa';
+var idade = 27;
+console.log(typeof idade); // usa-se o typeof para ver o tipo 
+
+//Concatenando strings
+var nome = 'Wanessa';
+var sobrenome = 'Loureiro';
+var nomeCompleto = nome + ' ' + sobrenome;
+console.log(nomeCompleto);
+
+// Concatenando strings com number
+var gols = 1000;
+var frase = 'Romário fez ' + gols + ' gols';
+console.log(frase); // O resultado da concatenção tem typeof string
+
+var texto = 'Aqui vai um \"teste"\ para exibir as aspas no meio do texto.';
+console.log(texto);
