38129e8b4b2748bab9804f65ba4837758481cf7a
WanessaT <wanessatlsousa@gmail.com>
2024-07-06 21:29:47 -0300

    Iniciando o curso: Aula e atividade  sobre variáveis

---
+++ variaveis/ex-variaveis.js
@@ -0,0 +1,18 @@
+// Declarar uma variável com o seu nome
+let nome = 'Wanessa Thaylanny';
+// Declarar uma variável com a sua idade
+let idade = 27;
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+let comidaFavorita;
+// Atribuir valor a sua comida favorita
+comidaFavorita = 'Pizza de Mussarela'
+
+// Declarar 5 variáveis diferentes sem valores
+let cidadeNascimento = 'Belem - PA',
+    nomeMae,
+    bairroOndeMora,
+    cursoFaculdade,
+    hobbie;
+
+console.log(nome, idade, comidaFavorita, cidadeNascimento)
---
+++ variaveis/script.js
@@ -0,0 +1,21 @@
+//Formas de declarar e exibir variáveis no console. Obs: Pode ser declarado usando var, let e const.
+
+
+var nome = 'Wanessa';
+var idade = 27;
+var possuiFaculdade = true;
+
+console.log(nome, idade, possuiFaculdade);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+
+console.log(totalPreco);
+
+var sobrenome = 'Thaylanny', cidade = 'Fortaleza';
+
+console.log(sobrenome, cidade)
+
+var semDefinir;
+console.log(semDefinir);
